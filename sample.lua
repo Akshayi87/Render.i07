325912370abb67c68c52494021bff2054830ef3e
Akshayi87 <katparaakshay87@gmail.com>
2026-05-01 18:29:09 +0530

    Add files via upload

---
+++ sample.lua
@@ -0,0 +1,12 @@
+-- Sample Lua Script for Akshu Admin
+-- This is a test script that can be uploaded to the server
+
+print("Hello from Akshu Admin Server!")
+print("Script loaded successfully")
+
+local function main()
+    print("Main function executed")
+    return true
+end
+
+main()
